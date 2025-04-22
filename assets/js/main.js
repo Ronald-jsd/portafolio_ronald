78977074436b8b7c259d24122e264ce6ae6d4d0a
Ronald Jaime <160687644+Ronald-jsd@users.noreply.github.com>
2025-04-22 01:08:53 -0500

    pull project

---
+++ assets/js/main.js
@@ -0,0 +1,45 @@
+/* ----- TYPING EFFECT ----- */
+var typingEffect = new Typed(".typedText", {
+    strings: ["Student &#127891;" ,"Developer &#128187;", "Back-end Jr. &#128736;", "Ronald! &#128522;"],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 2000
+})
+
+function filterProjects(category) {
+    // Obtener todos los proyectos
+    const projects = document.querySelectorAll('.project-container-box');
+
+    projects.forEach(project => {
+        if (project.classList.contains(category)) {
+            project.style.display = 'flex'; // Mostrar
+        } else {
+            project.style.display = 'none'; // Ocultar
+        }
+    });
+}
+
+// CURSOR
+document.addEventListener('mousemove', function (e) {
+    let smoke = document.createElement('div');
+    smoke.className = 'smoke';
+    smoke.style.left = `${e.pageX}px`;
+    smoke.style.top = `${e.pageY}px`;
+    document.body.appendChild(smoke);
+
+    setTimeout(() => {
+        smoke.remove();
+    }, 1000);
+});
+
+ /* ----- NAVIGATION BAR FUNCTION ----- */
+ function myMenuFunction() {
+   var menuBtn = document.getElementById("myNavMenu");
+
+   if (menuBtn.className === "nav-menu") {
+     menuBtn.className += " responsive";
+   } else {
+     menuBtn.className = "nav-menu";
+   }
+}
